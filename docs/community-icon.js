168f4f0bb53a8f6f9eaa58a79ff4a625ba8ff6fd
Kaityo1221 <rakugo100@gmail.com>
2026-09-24 07:50:32 +0900

    Refresh Suzuki cleanup cache for soot fade

--- docs/community-icon.js
+++ docs/community-icon.js
@@ -102,7 +102,7 @@ if(/(?:^|\/)stamp-rally\.html$/.test(location.pathname)){
  s.async=false;
  s.onload=()=>{
   const c=document.createElement("script");
-  c.src="./suzuki-native-cleanup-button.js?v=20260924-0732";
+  c.src="./suzuki-native-cleanup-button.js?v=20260924-0749";
   c.async=false;
   document.head.appendChild(c)
  };
