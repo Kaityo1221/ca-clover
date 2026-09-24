8fbef95bf71bd8a8d0be44e24221a7d6460372e6
Kaityo1221 <rakugo100@gmail.com>
2026-09-24 12:59:01 +0900

    Refresh Suzuki dragon image flow cache key

--- docs/community-icon.js
+++ docs/community-icon.js
@@ -98,7 +98,7 @@ window.CACommunityIcon={url,img,bind};
 installAdminIconHistoryFix();
 if(/(?:^|\/)stamp-rally\.html$/.test(location.pathname)){
  const s=document.createElement("script");
- s.src="./suzuki-native-flow.js?v=20260924-1145";
+ s.src="./suzuki-native-flow.js?v=20260924-1256";
  s.async=false;
  s.onload=()=>{
   const c=document.createElement("script");
