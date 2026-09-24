720c718411eabda112833af0b3bbc07433bd48e5
Kaityo1221 <rakugo100@gmail.com>
2026-09-24 18:36:53 +0900

    Refresh Suzuki repeated roar cache key

--- docs/community-icon.js
+++ docs/community-icon.js
@@ -98,7 +98,7 @@ window.CACommunityIcon={url,img,bind};
 installAdminIconHistoryFix();
 if(/(?:^|\/)stamp-rally\.html$/.test(location.pathname)){
  const s=document.createElement("script");
- s.src="./suzuki-native-flow.js?v=20260924-1827";
+ s.src="./suzuki-native-flow.js?v=20260924-1836";
  s.async=false;
  s.onload=()=>{
   const c=document.createElement("script");
